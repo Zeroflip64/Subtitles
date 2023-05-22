c85dd4eb4c528ff1e2e073cd0415156f8671ba6a
Zeroflip64 <122420589+Zeroflip64@users.noreply.github.com>
2023-05-22 23:18:43 +0700

    streamlit_sub.py

--- streamlit_sub.py
+++ streamlit_sub.py
@@ -44,37 +44,10 @@ import tempfile
 
 nlp = spacy.load("en_core_web_sm")
 
-def download_and_extract_model(model_url, model_dir='model'):
-    # Create temporary directory
-    os.makedirs(model_dir, exist_ok=True)
-    
-    # Download the model .tar.gz file
-    response = requests.get(model_url, stream=True)
-    tar_gz_path = os.path.join(tempfile.gettempdir(), 'model.tar.gz')
-    if os.path.exists(tar_gz_path):
-        st.write(f"File exists at {tar_gz_path}")
-    else:
-        st.write(f"No file found at {tar_gz_path}")
-    with open(tar_gz_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-    print(f"Model downloaded to {tar_gz_path}")
- 
-    # Extract the model files
-    with tarfile.open(tar_gz_path, 'r:gz') as tar:
-        tar.extractall(path=model_dir)
-    print(f"Model extracted to {model_dir}")
-
-model_url = 'https://drive.google.com/uc?id=1DIi2CgTSwz1I8bXJNVCxc9Vfo0x_uIXX'  # replace with your actual link
 
 
-download_and_extract_model(model_url) 
-
-
-model = hub.load('model')
-
+  module_url = "https://drive.google.com/file/d/1DIi2CgTSwz1I8bXJNVCxc9Vfo0x_uIXX/view?usp=share_link"
+  model = hub.load(module_url)
 
 language=['RUS','ENG']
 selected_len = st.multiselect('Язык/Lsnguage ?', language)
