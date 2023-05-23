08ed29ce52537189d27a13ee163fbba039f03e26
Zeroflip64 <122420589+Zeroflip64@users.noreply.github.com>
2023-05-23 23:14:29 +0700

    streamlit_sub.py

--- streamlit_sub.py
+++ streamlit_sub.py
@@ -49,7 +49,7 @@ if 'RUS' in selected_len:
       word_freqs = FreqDist(i.lower() for i in reuters.words())
       common_words = set(nltk_words.words())
       stop_words = set(stopwords.words('english'))
-      nlp = en_core_web_sm.load()
+      nlp = spacy.load('en_core_web_sm')
       
       def __init__(self, first):
           self.first = first
