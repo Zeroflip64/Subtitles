508d66477866b9c03b48c10f4939852fb7badc0e
Zeroflip64 <122420589+Zeroflip64@users.noreply.github.com>
2023-05-23 21:43:12 +0700

    streamlit_sub.py

--- streamlit_sub.py
+++ streamlit_sub.py
@@ -20,7 +20,7 @@ from catboost import CatBoostClassifier
 from sklearn.metrics import f1_score,accuracy_score
 import streamlit as st
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("https://github.com/Zeroflip64/Subtitles/blob/main/en_core_web_sm-3.1.0.tar.gz")
 
 
 
